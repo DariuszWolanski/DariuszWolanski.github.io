8a9db1e59afc0dae7e12b77e5a6288a58f9b1bab
DariuszWolanski <88981943+DariuszWolanski@users.noreply.github.com>
2022-02-09 20:19:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://https://kodilla-course-dariusz-wolansk.herokuapp.com/v1/task/';
+  var apiRoot = 'https://kodilla-course-dariusz-wolansk.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
